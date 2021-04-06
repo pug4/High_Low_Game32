c4ad357907ed5ea76a37f4777bd1e75986f5e7d6
pug4 <jayuprocks26@gmail.com>
2021-04-06 15:14:16 -0400

    Initial Commit

---
+++ High_Low_Game/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  High_Low_Game
+//
+//  Created by JOJO on 4/6/21.
+//  Copyright © 2021 Jayu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
